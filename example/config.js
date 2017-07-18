6060cb06f031456daf88c3c0e8800a8ad963db18
ziaochina <ziaochina@gmail.com>
2017-07-18 10:18:07 +0800

    zlj

---
+++ example/config.js
@@ -0,0 +1,18 @@
+import {Toast,Notification, Modal } from 'mk-component'
+
+var _options
+
+function config(options) {
+	_options = options
+	_options.targetDomId = 'app'
+	_options.startAppName = 'mk-app-login' 
+
+	_options.toast = Toast
+	_options.notification = Notification
+	_options.modal = Modal
+	return _options
+}
+
+config.getCurrent = () => _options
+
+export default config
