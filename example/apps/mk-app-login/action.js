ed3c322da53d7cbda08f78feb6397e5c9bc44018
ziaochina <ziaochina@gmail.com>
2017-09-15 11:06:36 +0800

    zlj

--- example/apps/mk-app-login/action.js
+++ example/apps/mk-app-login/action.js
@@ -46,7 +46,6 @@ class action {
     }
 
     goForgot = () =>{
-        debugger
         if(!this.config.apps['mk-app-forgot-password']){
             throw '请将这个应用加入到带mk-app-root和mk-app-forgot-password的网站中，跳转功能才能正常使用'
         }
--- example/apps/mk-app-login/webapi.js
+++ example/apps/mk-app-login/webapi.js
@@ -8,6 +8,29 @@ import { fetch } from 'mk-utils'
 
 export default {
     user: {
+        /**
+         * @api {POST} /v1/user/login 登录
+         * @apiName login
+         * @apiGroup webapi__user
+         * @apiPermission anyone
+         * @apiDescription 用于用户登录网站使用的接口
+         * @apiParam  {Object} option json对象
+         * @apiParamExample  {json} 请求示例 
+           {
+               "mobile" : "13334445556", //手机号
+               "password" : "1" //密码
+           }
+         * @apiSuccessExample {Json} 返回示例 
+           {
+                "result": true, //是否成功标志
+                "token": "1,13334445556,1,13334445556,,", //登录后token
+                //返回值
+                "value": {  
+                    "mobile": "13334445556",
+                    "password": "1"
+                }
+            }
+         */
         login: (option) => fetch.post('/v1/user/login', option)
     }
 }
